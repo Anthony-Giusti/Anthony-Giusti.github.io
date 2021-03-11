a04ace04bb64097d5f93173d691c5ae7abb96178
AlbatrossMissle <69980441+AlbatrossMissle@users.noreply.github.com>
2021-03-10 18:04:58 -0600

    CSS overhaul. Added loading spinner for video and favicon

--- js/data/portfolioData.js
+++ js/data/portfolioData.js
@@ -13,9 +13,9 @@ const porfolioData = [
     id: 2,
     name: "Build-A-Pizza",
     video:
-      "https://res.cloudinary.com/diby2nljo/video/upload/ac_none,c_crop,h_474,q_auto:eco,w_800/v1614293810/Portfolio/Video/portfoliovideo-pizza_z6goxu.mp4",
+      "https://res.cloudinary.com/diby2nljo/video/upload/ac_none,c_crop,h_474,q_auto:low,w_800/v1614293810/Portfolio/Video/portfoliovideo-pizza_z6goxu.mp4",
     description:
-      "For this project I made an React app similar to the Dominos pizza maker. You can add and remove items from the pizza and a pizza graphic will be a represetation of the pizza. I added some animations however I did not want them to he overwheling.",
+      "For this project I made an React app similar to the Domino's pizza maker. You can add and remove items from the pizza and a pizza image will update accordingly. I added some animations however I did not want them to be overwhelming.",
     liveLink: "https://pizza-maker-git-main.anthony-giusti.vercel.app/",
     gitHubLink: "https://github.com/Anthony-Giusti/pizza-maker",
   },
@@ -23,9 +23,9 @@ const porfolioData = [
     id: 3,
     name: "Spotify Charts",
     video:
-      "https://res.cloudinary.com/diby2nljo/video/upload/ac_none,c_crop,h_474,q_auto:eco,w_800/v1614276826/Portfolio/Video/Untitled_m1np8c.mp4",
+      "https://res.cloudinary.com/diby2nljo/video/upload/ac_none,c_crop,h_474,q_auto:low,w_800/v1614276826/Portfolio/Video/Untitled_m1np8c.mp4",
     description:
-      "This project was my first React app. There is a project on Codecademy Pro that used the Spotify API that I enjoyed so I wanted to make my own app with it. You can log in with your Spotify account and it will display information like your most played artists, tracks and genres as well as give you a few ways to sort the data.",
+      "This project was my first React app. There is a project on Codecademy Pro that uses the Spotify API and I enjoyed it so much I decided to make my own app with it. You can log in with your Spotify account and it will display information like your most played artists, tracks and genres as well as give you a few ways to sort the data.",
     liveLink: "https://spotifycharts.anthony-giusti.vercel.app/",
     gitHubLink: "https://github.com/Anthony-Giusti/spotifycharts",
   },
